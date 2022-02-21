3f067d7c3676d5c2335880058e478e9da99627e1
Harminder Virk <virk.officials@gmail.com>
2022-02-21 07:31:09 +0530

    feat: add footer

--- start/view.ts
+++ start/view.ts
@@ -17,3 +17,8 @@ View.global('icon', (filePath: string) => {
 View.global('image', (filePath: string) => {
   return View.GLOBALS.asset(`assets/images/${filePath}`)
 })
+
+View.global('filePathToUrl', (filePath: string) => {
+  const [, contentPath] = filePath.split('japa.dev/')
+  return `https://github.com/japa/japa.dev/tree/develop/${contentPath}`
+})
