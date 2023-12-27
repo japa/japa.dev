319a212232ac53ad57c5cb0cc3554bf13d0977a5
Harminder Virk <virk.officials@gmail.com>
2023-12-27 16:45:01 +0530

    style: format source code

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
-  plugins: [adonisjs({
-    entrypoints: ['./assets/app.js', './assets/app.css'],
-    reload: ['content/**/*', 'templates/**/*.edge']
-  })]
+  plugins: [
+    adonisjs({
+      entrypoints: ['./assets/app.js', './assets/app.css'],
+      reload: ['content/**/*', 'templates/**/*.edge'],
+    }),
+  ],
 })
